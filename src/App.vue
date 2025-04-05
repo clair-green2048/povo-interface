d31dc15de486a39e537a38f3ee1a351d58ae48f2
clair.green2048 <clair.green2048@gmail.com>
2025-04-05 13:35:56 -0400

    added structure and backend for Plans

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,10 @@
       @toggle-page="togglePage"
       @close-tab="closeTab">
     </TabsBar>
-    <PlansPage v-if="currentPage === 'CoursePlansPage'"></PlansPage>
+    <PlansPage v-if="currentPage === 'CoursePlansPage'" 
+      :coursePlans="coursePlans"
+      :selectedPlan="selectedPlan">
+    </PlansPage>
     <RegistrationPage v-if="currentPage === 'CourseRegistrationPage'"></RegistrationPage>
     <SchedulePage v-if="currentPage === 'SchedulePage'"></SchedulePage>
     <SearchPage v-if="currentPage === 'SearchPage'"></SearchPage>
@@ -26,6 +29,7 @@ import RegistrationPage from './components/RegistrationPage.vue';
 import SchedulePage from './components/SchedulePage.vue';
 import SearchPage from './components/SearchPage.vue';
 
+// ### TABS BAR ###
 const currentPage = ref<string>("HomePage");
 const openTabs = ref<Array<string>>([]);
 const currentTab = ref<string>("");
@@ -56,7 +60,61 @@ const closeTab = (tabName: string) => {
     currentPage.value = currentTab.value
   }
 }
+
+// ### PLANS ###
+interface Course {
+  name: string;
+  professor: string;
+  time: string;
+  dates: string;
+  location: string;
+}
+
+const coursePlans = ref<Record<string, Record<string, Course>>>({
+  "Heavy CS": {
+    "CSE 30311": {
+      name: "Theory of Computing",
+      professor: "David Chiang",
+      time: "2:00PM-3:15PM",
+      dates: "TTh",
+      location: "DeBartolo Hall 126"
+    },
+    "CSE 30124": {
+      name: "Introduction to Artificial Intelligence",
+      professor: "William Theisen",
+      time: "3:30PM-4-45PM",
+      dates: "MW",
+      location: "DeBartolo Hall 155"
+    }
+  },
+  "Major Reqs": {
+    "CSE 30311": {
+      name: "Theory of Computing",
+      professor: "David Chiang",
+      time: "2:00PM-3:15PM",
+      dates: "TTh",
+      location: "DeBartolo Hall 126"
+    },
+    "CSE 30341": {
+      name: "Operating System Principles",
+      professor: "Douglas Thain",
+      time: "9:30AM-10:45AM",
+      dates: "TTh",
+      location: "Pasquerilla Center 107"
+    },
+    "MATH 30750": {
+      name: "Real Analysis",
+      professor: "Qing Han",
+      time: "9:25AM-10:15AM",
+      dates: "MWF",
+      location: "Riley Hall 200"
+    }
+  }
+})
+
+const selectedPlan = ref<string>("Heavy CS");
 </script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
